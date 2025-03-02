5440daa93189c9b50bf00941d0d47ce94c39ebcb
JiantaoLin <1430208580@qq.com>
2025-03-02 16:12:31 +0800

    new

--- download_models.py
+++ download_models.py
@@ -15,6 +15,8 @@ zero123_plus_dir = os.path.join(ckpt_save_root, "zero123++")
 
 flux_lora_pth = hf_hub_download(repo_id="LTT/Kiss3DGen", filename="rgb_normal.safetensors", repo_type="model", local_dir="./checkpoint/flux_lora", local_dir_use_symlinks=False)
 
+redux_flux_lora_pth = hf_hub_download(repo_id="LTT/Kiss3DGen", filename="rgb_normal_redux.safetensors", repo_type="model", local_dir="./checkpoint/flux_lora", local_dir_use_symlinks=False)
+
 model_ckpt_path = hf_hub_download(repo_id="LTT/PRM", filename="final_ckpt.ckpt", repo_type="model", local_dir="./checkpoint/lrm", local_dir_use_symlinks=False)
 
 unet_ckpt_path = hf_hub_download(repo_id="LTT/Kiss3DGen", filename="flexgen.ckpt", repo_type="model", local_dir="./checkpoint/zero123++", local_dir_use_symlinks=False)
